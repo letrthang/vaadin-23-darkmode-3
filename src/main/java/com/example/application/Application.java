388f6ffb2d67239c9183a5abe31a25edba0a8b65
start.vaadin.com <no-reply@vaadin.com>
2022-06-10 02:42:10 +0000

    Generated project

---
+++ src/main/java/com/example/application/Application.java
@@ -0,0 +1,33 @@
+package com.example.application;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "vaadin-23-darkmode-3", variant = Lumo.DARK)
+@PWA(name = "Vaadin-23-Darkmode-3", shortName = "Vaadin-23-Darkmode-3", offlineResources = {})
+@Push
+@NpmPackage(value = "@adobe/lit-mobx", version = "2.0.0")
+@NpmPackage(value = "mobx", version = "^6.3.5")
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
